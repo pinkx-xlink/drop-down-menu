18ebf377db34dd810adbee77cfd0cbff3e624fd7
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-02 15:06:58 -0400

    Add event listener to dropdown button

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('click', e => {
+    const isDropdownButton = e.target.matches('[data-dropdown-button]')
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+    let currentDropdown
+    
+    is (isDropdownButton {
+
+    })
+})
